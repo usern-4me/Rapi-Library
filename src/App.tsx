c44e6082f2c21340126e1802d7fbfd9cd9b01950
Usern-4me <mazurkiewicz.leon@gmail.com>
2025-07-16 09:13:02 +0200

    Getting API Url from input

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
-//import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import Header from "./components/Header";
-//import Footer from "./components/Footer";
 import StartupScreen from "./components/StartupScreen";
-
 
 
 function App() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
-  
-  return (
-    <>
-      <Header onMenuClick={() => setSidebarOpen(open => !open)}/>
-      <StartupScreen></StartupScreen>
-    </>
-  );
+    const [selectedSource, setSelectedSource] = useState<string | null>(null);
+
+
+
+    let mainSite;
+    if (!selectedSource || selectedSource == '') {
+        mainSite = <StartupScreen onSelect={setSelectedSource} />;
+    } else {
+        mainSite = <>Gallery        W.I.P.</>; //change for gallery later
+    }
+
+    return (
+        <>
+            <Header onMenuClick={() => setSidebarOpen(open => !open)} />
+            {mainSite}
+        </>
+    );
 }
 
 export default App;
--- src/components/StartupScreen.tsx
+++ src/components/StartupScreen.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import "./StartupScreen.css";
 
-const StartupScreen = () =>{
-  return (
+
+
+const StartupScreen = ({ onSelect }: { onSelect: (source: string) => void }) =>{
+    const [customURL, setCustomURL] = useState('');
+
+return (
     <div className='StartupScreen'>
         <div className='preloaded-APIs'>
-            <a className='preloaded-APIs-button' href='#'>API #1</a>
-            <a className='preloaded-APIs-button' href='#'>API #2</a>
-            <a className='preloaded-APIs-button' href='#'>API #3</a>
+            <a className='preloaded-APIs-button' href='#' onClick={() => onSelect("API#1")}>API #1</a>
+            <a className='preloaded-APIs-button' href='#' onClick={() => onSelect("API#2")}>API #2</a>
+            <a className='preloaded-APIs-button' href='#' onClick={() => onSelect("API#3")}>API #3</a>
         </div>
         <div className='Custom-API'>
-            <input type='text' className='API-url-input' placeholder='https://picsum.photos/v2/list'></input><button>Button</button>
+            <input type='text' className='API-url-input' value={customURL} placeholder='https://picsum.photos/v2/list' onChange={(e) => setCustomURL(e.target.value)}></input><button onClick={() => onSelect(customURL)}>Button</button>
         </div>
-
     </div>
-  );
+);
 };
 
 export default StartupScreen;
